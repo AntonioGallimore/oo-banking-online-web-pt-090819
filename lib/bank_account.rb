1f468e1c967086ad738b9c16512c340a7c11b87d
Antonio Gallimore <antonio.gallimore@gmail.com>
2019-10-16 15:53:21 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,28 @@
 class BankAccount
+    attr_reader :name
+    attr_accessor :balance, :status
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+  
+  def deposit(deposit_amount)
+    self.balance += deposit_amount
+  end
+  
+  def display_balance
+    "Your balance is $#{balance}."
+  end
+  
+  def valid?
+    if
+      @status == ["open"] and @balance == (0+1)
+      else
+        !valid?
+   end
+  end
+  
 
 end
