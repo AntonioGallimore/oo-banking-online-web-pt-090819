69e53d221267eed437b791b592b199df370dd2b5
Antonio Gallimore <antonio.gallimore@gmail.com>
2019-10-16 16:18:53 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,8 +2,8 @@ class Transfer
   attr_accessor :amount, :sender, :status
   attr_reader :reciever
   
-  def initialize(name)
-    @name = name
+  def initialize(sender, reciever, amount)
+    @status = "pending"
   end
   
   
